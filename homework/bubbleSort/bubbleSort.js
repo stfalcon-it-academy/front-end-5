4b5195f06db6c4c4d8b5e5746b62072c13873a01
Sick-boy <office404error@gmail.com>
2018-04-05 18:19:28 +0300

    bubble

--- homework/bubbleSort/bubbleSort.js
+++ homework/bubbleSort/bubbleSort.js
@@ -1,10 +1,16 @@
 var arr = [1, 2, 7, 10, 6, 9, 3, 8, 5, 4, 2];
+var arrL = arr.length;
 
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i+1]) {
-        let temp = arr[i];
-        arr[i] = arr[i+1];
-        arr[i+1] = temp;
-        console.log(arr);
+
+for (let j = 0; j < arrL; j++) {
+    for (let i = 0; i < arrL - 1; i++) {
+        if (arr[i] > arr[i+1]) {
+            let temp = arr[i];
+            arr[i] = arr[i+1];
+            arr[i+1] = temp;
+        }
     }
+    console.log(arr);
 }
+
+
