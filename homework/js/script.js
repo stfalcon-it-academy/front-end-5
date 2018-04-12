18e307688bf46236d7ffa353cba90e5752cc7eb7
lucrat <35952848+lucrat@users.noreply.github.com>
2018-04-12 21:24:11 +0300

    fix_js

--- homework/js/script.js
+++ homework/js/script.js
@@ -1,22 +1,20 @@
-alert("Давай піднімемо твоє значення до степення");
-function funcPow() {
+function MPow(a,b) {
+    let pow=a;
+    for (i=0;i<b;i++){
+        pow=pow*a;
+    }
+ return pow;
+}
 let a=prompt("Введіть значення яке хочете піднести до степення");
 let b=prompt("Введіть степінь до якого хочете піднести значення");
-let x = Math.pow(a,b);
-    if (a==="") {
-        alert ("Повторно введіть значення");
-    funcPow ();
-    } else if (b==="") {
-        x=Math.pow(a,2);
-        alert(x);
-    }
-    else
-        alert(x);
-rep=confirm("Скористатися обрахункмами ще раз?");
-if (rep===true) {
-    funcPow();
-} else
-    document.write("Bye");
+if (a==="") {
+    alert ("Перезавантажте сторінку та повторно введіть значення");
+} else if (b==="") {
+    let x=a*a;
+    alert ("Квадрат значення"+a+"="+x);
 }
+else
+alert ("Рішення:"+MPow(a,b));
 
-funcPow ()
+
+
