40f872e59b370ffcb13699d57a72ca016d2036da
dmytrokukharuk <dimakyh@ukr.net>
2018-06-19 15:45:11 +0300

    fix on site

---
+++ src/js/main.js
@@ -0,0 +1,223 @@
+'use strict';
+
+//init slider Slick
+
+$(function () {
+    $('.some_slider-js').slick({
+        arrows: false,
+        initialSlide: 0,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        speed: 2000
+    });
+});
+
+
+// script for Google map color
+
+function initMap() {
+    var mySchool = {lat: 49.432614, lng: 27.000129};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 14,
+        center: mySchool
+    });
+
+    var markerIcon = '../img/icons/marker.png';
+    var marker = new google.maps.Marker({
+        position: mySchool,
+        map: map,
+        icon: markerIcon
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: 'Название компании<br/>Адрес',
+    });
+
+    var infowindow = new google.maps.InfoWindow({
+        content: 'Sarajevo<br/>2345 Setwant natrer, 1234,<br/>Washington. United States.',
+    });
+    marker.addListener('click', function () {
+        infowindow.open(map, marker);
+    });
+    var styledMapType = new google.maps.StyledMapType(
+        [
+            {
+                "elementType": "geometry",
+                "stylers": [{"color": "#fdfdfd"}]
+            },
+            {
+                "elementType": "labels.icon",
+                "stylers": [{"visibility": "off"}]
+            },
+            {
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#616161"}]
+            },
+            {
+                "elementType": "labels.text.stroke",
+                "stylers": [{"color": "#f5f5f5"}]
+            },
+            {
+                "featureType": "administrative.land_parcel",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#bdbdbd"}]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "geometry",
+                "stylers": [{"color": "#eeeeee"}]
+            },
+            {
+                "featureType": "poi",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#757575"}]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "geometry",
+                "stylers": [{"color": "#e5e5e5"}]
+            },
+            {
+                "featureType": "poi.park",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#9e9e9e"}]
+            },
+            {
+                "featureType": "road",
+                "elementType": "geometry",
+                "stylers": [{"color": "#efefef"}]
+            },
+            {
+                "featureType": "road.arterial",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#757575"}]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "geometry",
+                "stylers": [{"color": "#dadada"}]
+            },
+            {
+                "featureType": "road.highway",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#616161"}]
+            },
+            {
+                "featureType": "road.local",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#9e9e9e"}]
+            },
+            {
+                "featureType": "transit.line",
+                "elementType": "geometry",
+                "stylers": [{"color": "#e5e5e5"}]
+            },
+            {
+                "featureType": "transit.station",
+                "elementType": "geometry",
+                "stylers": [{"color": "#eeeeee"}]
+            },
+            {
+                "featureType": "water",
+                "elementType": "geometry",
+                "stylers": [{"color": "#c9c9c9"}]
+            },
+            {
+                "featureType": "water",
+                "elementType": "labels.text.fill",
+                "stylers": [{"color": "#9e9e9e"}]
+            }
+        ],
+        {name: 'Styled Map'}
+    );
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+};
+
+
+// init Isotope
+
+var $grid = $('.grid').isotope({
+    itemSelector: '.element-item',
+    layoutMode: 'fitRows',
+    getSortData: {
+        category: '[data-category]',
+
+    }
+});
+
+// filter functions
+var filterFns = {
+    // show if number is greater than 50
+    numberGreaterThan50: function () {
+        var number = $(this).find('.number').text();
+        return parseInt(number, 10) > 50;
+    }
+};
+
+// bind filter button click
+$('#filters').on('click', '.button', function () {
+    var filterValue = $(this).attr('data-filter');
+    // use filterFn if matches value
+    filterValue = filterFns[filterValue] || filterValue;
+    $grid.isotope({filter: filterValue});
+});
+
+
+// change is-checked class on buttons
+$('.button-group').each(function (i, buttonGroup) {
+    var $buttonGroup = $(buttonGroup);
+    $buttonGroup.on('click', '.button', function () {
+        $buttonGroup.find('.is-checked').removeClass('is-checked');
+        $(this).addClass('is-checked');
+    });
+});
+
+
+// menu fixed on top
+
+$(window).scroll(function () {
+    var header = $('.header_top');
+
+    if ($(window).scrollTop() >= 200) {
+        header.addClass('fixed-header');
+    }
+    else {
+        header.removeClass('fixed-header');
+    }
+});
+
+
+//burger menu
+
+$(function () {
+    $('.burger_trigger, .header_menu').click(function () {
+        $('.header_menu, .burger_menu').toggleClass('menu_opened');
+    });
+    $(document).click(function (e) {
+        if ($(e.target).closest('.burger_trigger').length) return;
+        $('.header_menu, .burger_menu').removeClass('menu_opened');
+        e.stopPropagation();
+    })
+
+});
+
+
+//smooth scroll for menu anchor
+
+$(document).ready(function(){
+
+    const HEIGHT_HEADER = 46;
+
+    $(".header_menu").on("click","a", function (event) {
+        event.preventDefault();
+        let id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top - HEIGHT_HEADER}, 1500);
+        return false;
+    });
+});
+
